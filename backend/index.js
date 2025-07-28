91c4d75134cab411129b4fe5bb025c160d3a1d3a
sidharthsharma144 <sidharthsharma17240@gmail.com>
2025-07-28 22:59:37 +0530

    add note url working

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const config = require("./config.json")
 const mongoose = require("mongoose")
 const User = require("./models/user.model");
-
+const Note = require("./models/note.model");
 
 const express = require("express");
 const cors = require ("cors");
@@ -116,7 +116,39 @@ app.post("/login", async (req,res)=>{
 )
 // for adding notes
 app.post("/add-note", authenticationToken, async (req,res)=>{
+    const{title,content,tags}=req.body;
+    const{user}=req.user;
+    if(!title){
+        return res.status(400).json({error:true,message:"Title is required"});
+
+    }
+    if(!content){
+        return res.status(400).json({error:true, message:"Content is required"})
+    }
+
+    try{
+        const note = new Note({
+            title,
+            content,
+            tags:tags||[],
+            userId:user._id,
+        })
+
+        await note.save();
+        return res.json({
+            error:false,
+            note,
+            message:"Note added succesfully ",
+        })
+    }
+        catch(error){
+            return res.status(500).json({
+                error:true,
+                message:"Internal Server Error",
+            })
+        }
     
+
 })
 app.listen(8000);
 
