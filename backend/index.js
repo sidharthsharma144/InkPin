f6790af371ba79095db722da4e3e379ab1b8f299
sidharthsharma144 <sidharthsharma17240@gmail.com>
2025-07-29 14:59:01 +0530

    delete note url working

--- backend/index.js
+++ backend/index.js
@@ -184,7 +184,58 @@ app.put("/edit-note/:noteId", authenticationToken, async(req,res)=>{
         });
     }
 });
+// GET ALL NOTES
+app.get("/get-all-notes/", authenticationToken, async(req,res)=>{
+    const {user}= req.user;
+    try{
+        const notes = await Note.find({userId:user._id}).sort({
+            isPinned: -1
+        })
 
+        return res.json({
+            error:false,
+            notes,
+            message:"All Notes retrieved succesfully",
+        }) 
+
+    }
+    catch(error){
+        return res.status(500).json({
+            error:true,
+            message:"Internal Server Error",
+        })
+    }
+
+})
+// DELETE NOTE
+app.delete("/delete-note/:noteId", authenticationToken, async(req,res)=>{
+    const noteId=req.params.noteId;
+    const{user}=req.user;
+
+    try{
+        const note = await Note.findOne({_id:noteId, userId:user._id})
+
+        if(!note){
+            return res.status(400).json({error:true,message:"Note Not Found"});
+        }
+        await Note.deleteOne({_id:noteId, userId:user._id})
+        return res.json({
+            error:false,
+            message:"Note deleted succesfully"
+        })
+    }
+    catch(error){
+        return res.status(500).json({
+            error:true,
+            message:"Internal Server Error",
+        })
+    }
+
+
+}) 
+//UPDATE NOTE PINNED
+app.delete("/delete-note/:noteId", authenticationToken, async(req,res)=>{
+})
 app.listen(8000);
 
 module.exports=app;
